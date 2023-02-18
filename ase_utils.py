2509c3f21ea1091c0b739e793fbb8d3e3d65c2f8
M-asaki-K <47930865+M-asaki-K@users.noreply.github.com>
2023-02-18 12:19:38 +0900

    warningsを無視するように修正、また解説の一部を修正。

--- ase_utils.py
+++ ase_utils.py
@@ -213,37 +213,6 @@ class OCPCalculator(Calculator):
             def numeric_stress(atoms, d=1e-6, voigt=True):
                 import numpy as np
                 stress = np.zeros((3, 3), dtype=float)
-
-                cell = atoms.cell.copy()
-                V = atoms.get_volume()
-                for i in range(3):
-                    x = np.eye(3)
-                    x[i, i] += d
-                    atoms.set_cell(np.dot(cell, x), scale_atoms=True)
-                    eplus = self.results["energy"]
-
-                    x[i, i] -= 2 * d
-                    atoms.set_cell(np.dot(cell, x), scale_atoms=True)
-                    eminus = self.results["energy"]
-
-                    stress[i, i] = (eplus - eminus) / (2 * d * V)
-                    x[i, i] += d
-
-                    j = i - 2
-                    x[i, j] = d
-                    x[j, i] = d
-                    atoms.set_cell(np.dot(cell, x), scale_atoms=True)
-                    eplus = self.results["energy"]
-
-                    x[i, j] = -d
-                    x[j, i] = -d
-                    atoms.set_cell(np.dot(cell, x), scale_atoms=True)
-                    eminus = self.results["energy"]
-
-                    stress[i, j] = (eplus - eminus) / (4 * d * V)
-                    stress[j, i] = stress[i, j]
-                atoms.set_cell(cell, scale_atoms=True)
-
                 if voigt:
                     return stress.flat[[0, 4, 8, 5, 2, 1]]
                 else:
